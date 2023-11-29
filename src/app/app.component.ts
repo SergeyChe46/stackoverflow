fe6d7c609a645c20cfc7c70c9905a688f6c205c8
Sergey Chevychelov <sergeyche46@mail.ru>
2023-11-29 18:06:15 +0300

    ОтображаетсЯ список постов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from './helpers/services/posts.service';
+import { Post } from './model/post.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stackoverflow';
+
+  constructor(private postsService: PostsService) {}
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  posts: Post[] = [
+    // {
+    //   id: 1,
+    //   title: 'His mother had always taught him',
+    //   body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
+    //   userId: 9,
+    //   tags: ['history', 'american', 'crime'],
+    //   reactions: 2,
+    // },
+  ];
+  /**
+   * Получает посты.
+   * @param limit
+   * @param skip
+   */
+  getAll(limit: number = 0, skip: number = 0) {
+    this.postsService.getAll(limit, skip).subscribe({
+      next: (result: any) => {
+        (this.posts = result['posts']), console.log(result);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
   menuItem: string[] = ['Main', 'About', 'Contacts'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './component/post/post.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
